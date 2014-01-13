70a2cc0830536679c971fcad2f78df55cc4e3bc7
Sebastian Gonzalez <sebagonz91@gmail.com>
2014-01-13 21:09:31 +0100

    Making code shorter

--- wiki.sh
+++ wiki.sh
@@ -3,8 +3,7 @@ LANG="en"
 if [[ -z $1 ]]; then
   echo -e "No argument given. \nUsage: wiki QUERY "
 else
-  all_args=$*
-  var=$(echo $all_args | sed 's/ /_/g') #Transforms 'One Time' to 'One_Time'
+  var=$(echo $* | sed 's/ /_/g') #Transforms 'One Time' to 'One_Time'
   wiki_data=$(curl -s "http://"$LANG".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&titles="$var"&redirects" | jq '.query.pages | to_entries[0] | .value.extract')
   data=$(echo $wiki_data | sed 's/\\\"/"/g') #Eliminates \" characters
   echo -e ${data:1:${#data}-2}
