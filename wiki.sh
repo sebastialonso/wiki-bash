09460a5a528f5121a07908d2540fae8d3a8ef8b0
Sebastian Gonzalez <sebagonz91@gmail.com>
2014-01-13 20:40:39 +0100

    Removes '"' characters, leaving only quotes

--- wiki.sh
+++ wiki.sh
@@ -3,8 +3,8 @@ LANG="en"
 if [[ -z $1 ]]; then
   echo -e "No argument given. \nUsage: wiki QUERY "
 else
-  var=$(echo $* | sed 's/ /_/g')
+  var=$(echo $* | sed 's/ /_/g') #Transforms 'One Time' to 'One_Time'
   wiki_data=$(curl -s "http://"$LANG".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&titles="$var"&redirects" | jq '.query.pages | to_entries[0] | .value.extract')
-  #echo "http://"$LANG".wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&titles="$var"&redirects"
-  echo -e ${wiki_data:1:${#wiki_data}-2}
+  data=$(echo $wiki_data | sed 's/\\\"/"/g') #Eliminates \" characters
+  echo -e ${data:1:${#data}-2}
 fi
